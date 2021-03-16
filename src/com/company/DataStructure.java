213c164e7fc8224e92a1043f0465afdc7358fc47
623588277@qq.com <62358877@qq.com>
2021-03-16 18:53:19 +0800

    递归

--- src/com/company/DataStructure.java
+++ src/com/company/DataStructure.java
@@ -2,9 +2,13 @@ package com.company;
 
 public class DataStructure {
 
-    public void ss(){
-        System.out.println();
-        System.out.println();
-        System.out.println();
+    public static void main(String[] args) {
+        System.out.println(f(1));
+    }
+    public static int f(int x){
+        if(x == 0){
+            return 0;
+        }
+        return 2*f(x-1)+x*x;
     }
 }
